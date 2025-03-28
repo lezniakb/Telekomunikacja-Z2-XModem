4e30ad4731f2466cb37e608b268c0be42b2ffbf4
Bartosz <251574@edu.p.lodz.pl>
2025-03-28 21:39:53 +0100

    Utworzenie głównego main

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import serial
+import time
+import sys
+
 # definicje znakow (zapisane w hex)
 SOH = b'\x01'       # poczatek blok
 EOT = b'\x04'       # koniec transmisji
@@ -31,3 +35,70 @@ def obliczCRC(dane):
                 # jesli nie, to przesuwamy crc o 1 w lewo i ograniczamy wynik do 16 bitow dzieki masce 0xFFFF
                 crc = (crc << 1) & 0xFFFF
     return crc
+
+def dopelnijDane(dane):
+    # dopelnij blok danych do 128 bajtow znakiem CTRL-Z (bajtowo: 0x1A)
+    dopelnienie = b"\x1A"
+    dlugosc = len(dane)
+    if dlugosc < BLOCK_SIZE:
+        dane += dopelnienie * (BLOCK_SIZE - dlugosc)
+    return dane
+
+
+# main
+port = ""
+while port.startswith("COM") != True:
+    port = input("Podaj wybrany port szeregowy (np. COM10): ")
+    port = port.strip().upper()
+
+try:
+    baudrate = int(input("Podaj prędkość transmisji (domyślnie 9600): ").strip())
+except ValueError:
+    baudrate = 9600
+    print(f"Podano niepoprawną wartość. Ustawiono baudrate={baudrate}")
+
+try:
+    ser = serial.Serial(port, baudrate, timeout=15)
+except Exception as e:
+    print("Wystąpił błąd przy otwieraniu porta:", e)
+    sys.exit(1)
+
+while True:
+    print("--------------\nMenu Główne:\n"
+    "1. Nadaj wiadomość\n"
+    "2. Odbierz wiadomość\n"
+    "3. Opuść program")
+    wybor = input("Wybierz opcję: ").strip()
+    if wybor == "1":
+        trybCRC = input("Użyć trybu CRC? [tak/nie]: ")
+        trybCRC = trybCRC.strip().lower()
+        if trybCRC == "t" or trybCRC == "tak":
+            print("Wybrano tryb CRC")
+            uzywajCRC = True
+            print("Wybrano tryb sumy kontrolnej")
+        else:
+            uzywajCRC = False
+        message = input("Podaj wiadomość do wysłania: ")
+        print("xmodem_send_message(ser, message, use_crc=use_crc)")
+        #xmodem_send_message(ser, message, use_crc=use_crc)
+
+    elif wybor == "2":
+        trybCRC = input("Użyć trybu CRC? [tak/nie]: ")
+        trybCRC = trybCRC.strip().lower()
+        if trybCRC == "t" or trybCRC == "tak":
+            print("Wybrano tryb CRC")
+            uzywajCRC = True
+        else:
+            print("Wybrano tryb sumy kontrolnej")
+            uzywajCRC = False
+        print("xmodem_receive_message(ser, use_crc=use_crc)")
+        #xmodem_receive_message(ser, use_crc=use_crc)
+
+    elif wybor == "3":
+        break
+
+    else:
+        print("Wybrano niepoprawną opcję!")
+
+ser.close()
+input("Wciśnij enter, aby zakończyć.")
